87b1869f8938c73c7ad1388a13fb25b0f8e64da7
CeshiaMendoza <43767658+CeshiaMendoza@users.noreply.github.com>
2018-10-03 17:51:49 -0500

    Add files via upload

---
+++ romanos/README.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package romanos;
+
+/**
+ *
+ * @author CyA
+ */
+public class README {
+    /*
+    Al ejecutar el programa se pedirá el ingreso de un número
+    entre 1 y 1000 para convertirlo en romano
+    al ingresarlo y dar enter dará la respuesta.
+    
+    Ciertamente el código esta muy largo ya que utilice puros condicionales
+    
+    */
+}
