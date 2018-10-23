6a2423a9200a3d20175689e1d4feac5ae572d781
CeshiaMendoza <43767658+CeshiaMendoza@users.noreply.github.com>
2018-10-23 16:03:27 -0500

    Update cubo.java

--- objetos/cubo.java
+++ objetos/cubo.java
@@ -4,18 +4,6 @@ package objetos;
 import java.io.*;
 import java.util.*;
 public class cubo extends Objetos {
-    /*
-    public cubo(int lado) throws IOException{
-   
-    int volumen;
-    int perimetro;
-    
-    volumen = lado * lado * lado;
-    System.out.println("El volumen es: " + volumen);
-    
-    perimetro = lado * 12;
-    System.out.println("El perimetro es: " + perimetro);
-    */
     
     int volumen;
     int perimetro;
